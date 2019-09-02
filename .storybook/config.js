7f7deee316f12cb20377e306b2369f39cac75c13
Alexander K <alukos@yandex.ru>
2019-09-02 18:31:40 +0300

    SB config: stories near w/ components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/vue';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+//const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../src', true, /.stories.(j|t)s$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
