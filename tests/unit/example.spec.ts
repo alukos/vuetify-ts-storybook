9769d98d2199673f839a3dbbd3eff212560a8c95
Alexander K <alukos@yandex.ru>
2019-09-02 17:13:48 +0300

    example.spec.ts: fix for h1 in wrapper

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,10 +3,10 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
+    const msg = 'Welcome to Vuetify';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
-    });
+    }).find('h1');
     expect(wrapper.text()).toMatch(msg);
   });
 });
