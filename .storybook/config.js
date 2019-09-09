fa59b0029d2cd673df694f78e2777f39fe45d29b
Alexander K <alukos@yandex.ru>
2019-09-09 12:38:24 +0300

    .storybook/config.js: register VApp

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
+import Vue from 'vue';
 import { configure } from '@storybook/vue';
+import { VApp } from 'vuetify/lib';
 
-import '../src/plugins/vuetify';
+Vue.component('v-app', VApp)
 
 // automatically import all files ending in *.stories.js
 //const req = require.context('../stories', true, /\.stories\.js$/);
