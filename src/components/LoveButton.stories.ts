0e61c7b7c9c1874dd43a145cf305f0b6f11b54c9
Alexander K <alukos@yandex.ru>
2019-09-09 13:29:05 +0300

    LoveButton.stories.ts: separate appDecorator

--- src/components/LoveButton.stories.ts
+++ src/components/LoveButton.stories.ts
@@ -3,11 +3,21 @@ import vuetify from '../plugins/vuetify';
 
 import LoveButton from './LoveButton.vue';
 
+const appDecorator = () => ({
+  vuetify,
+  template: `
+    <v-app>
+      <div style="background-color: rgb(134, 212, 226); padding: 20px; width: 100%; height: 100%;">
+        <v-content>
+          <story/>
+        </v-content>
+      </div>
+    </v-app>
+  `
+})
+
 storiesOf('LoveButton', module)
-  .addDecorator(() => ({
-    vuetify,
-    template: '<v-app id="blackship-admin"><story/></v-app>'
-  }))
+  .addDecorator(appDecorator)
   .add('default', () => ({
     components: { LoveButton },
     template: `<love-button love="vue"/>`,
