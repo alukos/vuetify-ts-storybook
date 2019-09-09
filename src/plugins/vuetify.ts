a7616456890b90111f4ee3c2ef7f67e45a6f14ce
Alexander K <alukos@yandex.ru>
2019-09-09 14:02:48 +0300

    plugins/vuetify.ts: add theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,5 +4,18 @@ import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
+    },
+  },
   iconfont: 'md',
 });
